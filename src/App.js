5c1f412be3ab9612edc644a1c5b783b080d87df0
Alberto Martínez <alberto@a81.biz>
2024-10-05 23:43:52 -0600

    FindMinAndMaxInArray

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import AnagramCheck from './infografias/AnagramCheck';
 import CheckPalindrome from './infografias/CheckPalindrome';
 import FindDuplicatesInArray from './infografias/FindDuplicatesInArray';
-import FindMinimumAndMaximumInArray from './infografias/FindMinimumAndMaximumInArray';
+import FindMinAndMaxInArray from './infografias/FindMinAndMaxInArray';
 import ReverseAString from './infografias/ReverseAString';
 import StringLength from './infografias/StringLength';
 import TwoSumProblem from './infografias/TwoSumProblem';
@@ -19,7 +19,7 @@ function App() {
         <Route path="/Anagram_Check" element={<AnagramCheck />} /> {}
         <Route path="/Check_Palindrome" element={<CheckPalindrome />} /> {}
         <Route path="/Find_Duplicates_in_an_Array" element={<FindDuplicatesInArray />} /> {}
-        <Route path="/Find_Minimum_and_Maximum_in_Array" element={<FindMinimumAndMaximumInArray />} /> {}
+        <Route path="/Find_Minimum_and_Maximum_in_Array" element={<FindMinAndMaxInArray />} /> {}
         <Route path="/Reverse_a_String" element={<ReverseAString />} /> {}
         <Route path="/String_Length" element={<StringLength />} /> {}
         <Route path="/Two_Sum_Problem" element={<TwoSumProblem />} /> {}
