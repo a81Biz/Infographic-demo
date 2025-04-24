bf00fc2c3711008626f36ad6b9f55564a38bbb34
Alberto Martínez <alberto@a81.biz>
2025-04-24 15:23:22 -0600

    auto index

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home'; 
-import AnagramCheck from './infografias/AnagramCheck';
-import CheckPalindrome from './infografias/CheckPalindrome';
-import FindDuplicatesInArray from './infografias/FindDuplicatesInArray';
-import FindMinAndMaxInArray from './infografias/FindMinAndMaxInArray';
-import ReverseAString from './infografias/ReverseAString';
-import StringLength from './infografias/StringLength';
-import TwoSumProblem from './infografias/TwoSumProblem';
-import RemoveDuplicatesForSortedArray from './infografias/RemoveDuplicatesForSortedArray';
-import BubbleSort from './infografias/BubbleSort';
-import LinearSearch from './infografias/LinearSearch';
-import SimpleSortingInSQL from './infografias/SimpleSortingInSQL';
-import FilteringInMongoDB from './infografias/FilteringInMongoDB';
-import PrimeNumberCheck from './infografias/PrimeNumberCheck';
-import CICDFlowDiagram from './infografias/CICDFlowDiagram';
+import Home from './components/Home';
+import routes from './routes';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {}
-        <Route path="/Anagram_Check" element={<AnagramCheck />} /> {}
-        <Route path="/Check_Palindrome" element={<CheckPalindrome />} /> {}
-        <Route path="/Find_Duplicates_in_an_Array" element={<FindDuplicatesInArray />} /> {}
-        <Route path="/Find_Minimum_and_Maximum_in_Array" element={<FindMinAndMaxInArray />} /> {}
-        <Route path="/Reverse_a_String" element={<ReverseAString />} /> {}
-        <Route path="/String_Length" element={<StringLength />} /> {}
-        <Route path="/Two_Sum_Problem" element={<TwoSumProblem />} /> {}
-        <Route path="/Remove_Duplicates_For_Sorted_Array" element={<RemoveDuplicatesForSortedArray />} /> {}
-        <Route path="/Bubble_Sort" element={<BubbleSort />} /> {}
-        <Route path="/Linear_Search" element={<LinearSearch />} /> {}
-        <Route path="/Simple_Sorting_In_SQL" element={<SimpleSortingInSQL />} /> {}
-        <Route path="/Filtering_In_MongoDB" element={<FilteringInMongoDB />} /> {}
-        <Route path="/Prime_Number_Check" element={<PrimeNumberCheck />} /> {}
-        <Route path='/CI_CD_Flow' element={<CICDFlowDiagram/>}/>{}
+        <Route path="/" element={<Home />} />
+        {routes.map((r, i) => (
+          <Route key={i} path={r.path} element={<r.component />} />
+        ))}
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../routes';
 
 function Home() {
   useEffect(() => {
@@ -6,8 +8,20 @@ function Home() {
   }, []);
 
   return (
-    <div style={{ height: '100vh', overflow: 'hidden' }}>
-        <p>Home Infografías</p>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Infografías disponibles</h1>
+      <ul className="grid grid-cols-2 gap-4">
+        {routes.map((r, idx) => (
+          <li key={idx}>
+            <Link
+              to={r.path}
+              className="block p-4 border rounded hover:bg-gray-100 transition"
+            >
+              {r.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
